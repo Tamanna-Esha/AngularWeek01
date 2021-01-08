9793ce403bc6cc86e279429de16e04785063ba27
Tamanna-Esha <mimtamannarahman@gmail.com>
2021-01-08 15:36:27 +0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
